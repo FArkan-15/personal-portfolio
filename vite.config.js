1f6967dad3ca09f0d919883dd81c7e0ebb5f6777
Falih Arkan Al Fathani Rasendriya <arkanrasendriya15@gmail.com>
2026-05-01 21:21:26 +0700

    fix image name contact illustration

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
     vueDevTools(),
     tailwindcss(),
   ],
+  base: '/personal-portfolio/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
